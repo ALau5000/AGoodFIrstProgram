4d30615b79a24254d55c2d08b764cac32abb4c5b
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-16 10:28:48 -0500

    Create AGoodFirstProgam.java

---
+++ AGoodFirstProgam.java
@@ -0,0 +1,15 @@
+public class AGoodFirstProgram {
+
+	public static void main(String[] args) {
+
+		System.out.println("I love bacon.");
+		System.out.println("And Cheeseburgers.");
+		System.out.println("And hot dogs.");
+		System.out.println("And bacon again.");
+		System.out.println("And pizza.");
+		System.out.println("I \"love\" hot dogs.");
+	}
+
+}	
+	
+    
