1e55705341e141994dd425b1815e5d5736360384
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-16 11:00:38 -0500

    Create CommentsAndSlashes.java

---
+++ CommentsAndSlashes.java
@@ -0,0 +1,15 @@
+public class CommentsAndSlashes {
+  
+   public void static main(String[] args) {
+   
+   //Yesterday I ate a sandwich and then I went to the supermarket to buy some bread
+
+      System.out.println("My dog is actually a cat.");
+      
+   //But I ended up having to steal it because I didn't have enough money 
+   
+      System.out.println("And my cat is actually disguised as an elephant.");
+      
+    }
+    
+}     
