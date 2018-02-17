7b7c1b252648034bfdb061cb1a4e192509f18c3e
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-17 14:58:52 -0500

    Create YourInitials.java

---
+++ YourInitials.java
@@ -0,0 +1,14 @@
+public class YourInitials {
+
+  public static void main(String[] args) {
+      System.out.println("        A           L               ");
+      System.out.println("       A A          L               ");
+      System.out.println("      A   A         L               ");
+      System.out.println("     A     A        L               ");
+      System.out.println("    A A A A A       L               ");
+      System.out.println("   A         A      L               ");
+      System.out.println("  A           A     L               ");
+      System.out.println(" A             A    L               ");
+      System.out.println("A               A   L L L L L L L L ");
+  }
+}
