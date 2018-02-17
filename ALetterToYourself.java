26718304bafd239158b44fff30f8e38a33bd92fc
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-17 14:41:03 -0500

    Update ALetterToYourself.java

--- ALetterToYourself.java
+++ ALetterToYourself.java
@@ -1,17 +1,17 @@
 public class ALetterToYourself {
    
   public static void main(String[] args) {
-    System.output.println("+--------------------------------------------------------+
-    System.output.println("|                                                ####    |
-    System.output.println("|                                                ####    |
-    System.output.println("|                                                ####    |
-    System.output.println("|                                                        |
-    System.output.println("|                                                        |
-    System.output.println("|                              Alvin Lau                 |
-    System.output.println("|                              123 Sesame Street         |
-    System.output.println("|                              Sesame, SSME 12345        |
-    System.output.println("|                                                        |
-    System.output.println("+--------------------------------------------------------+
+    System.output.println("+--------------------------------------------------------+");
+    System.output.println("|                                                ####    |");
+    System.output.println("|                                                ####    |");
+    System.output.println("|                                                ####    |");
+    System.output.println("|                                                        |");
+    System.output.println("|                                                        |");
+    System.output.println("|                              Alvin Lau                 |");
+    System.output.println("|                              123 Sesame Street         |");
+    System.output.println("|                              Sesame, SSME 12345        |");
+    System.output.println("|                                                        |");
+    System.output.println("+--------------------------------------------------------+");
   }
 }
     
