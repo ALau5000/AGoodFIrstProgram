d5192e9e660545e3b940b82ff264d89dc90ee5c0
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-17 14:50:52 -0500

    Create ALetterToYourself.java

---
+++ ALetterToYourself.java
@@ -0,0 +1,17 @@
+public class ALetterToYourself {
+   
+  public static void main(String[] args) {
+    System.out.println("+--------------------------------------------------------+");
+    System.out.println("|                                                ####    |");
+    System.out.println("|                                                ####    |");
+    System.out.println("|                                                ####    |");
+    System.out.println("|                                                        |");
+    System.out.println("|                                                        |");
+    System.out.println("|                              Alvin Lau                 |");
+    System.out.println("|                              123 Sesame Street         |");
+    System.out.println("|                              Sesame, SSME 12345        |");
+    System.out.println("|                                                        |");
+    System.out.println("+--------------------------------------------------------+");
+  }
+}
+    
