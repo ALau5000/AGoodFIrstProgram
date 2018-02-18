980c799330692bb62813827430992d53f19453e8
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-17 22:51:24 -0500

    Create NumbersAndMath.java

---
+++ NumbersAndMath.java
@@ -0,0 +1,16 @@
+public class NumbersAndMath {
+
+    public static void main(String[] args) {
+        System.out.println("I will count the chicken nuggets I will be consuming:");
+        System.out.println("*munch munch munch*    " + (70 + 123) + " nuggets down!");
+        System.out.println("Now let's see how much I can drink:");
+        System.out.println("*slurpslurpslurp*      " + (100000/10000) + " bottles of coke!");
+        System.out.println("And finally my favourite, which is dessert:");
+        System.out.println("*NOMNOMNOMNOMNOM*      " + ((3 + 5) / (1 + 2 + 5 - 2 - 1 + 3)) + " cupcake!");
+        System.out.println("I'm just curious, is 5 bigger than 1000?  " + (5 > 1000));
+        System.out.println("Oh, I see, then 5 must be bigger than 543678. Is it?");
+        System.out.println("Are you okay buddy? It's " + (5 > 543678));
+        System.out.println("Well, is 88 greater or equal to 888. I think it's equal since both numbers have 2 eights!:)");
+        System.out.println("I think you need to go back to kindergarten kiddo cause once again, it's " + (88 >= 888) + ". It's not even greater either...");
+    }
+}
