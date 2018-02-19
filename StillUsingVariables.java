38c1c5c7b405b7834e9b9ef483fe416f437d1b96
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-19 03:13:01 -0500

    Create StillUsingVariables.java

---
+++ StillUsingVariables.java
@@ -0,0 +1,14 @@
+public class StillUsingVariables {
+    
+    public static void main(String[] args) {
+        
+        int gradYear;
+        String myName;
+        
+        gradYear = 2019;
+        myName = "Bobby Joe";
+        
+        System.out.println("My name is " + (myName) + " and I'll graduate in " + (gradYear) + ".");
+        
+    }
+}
