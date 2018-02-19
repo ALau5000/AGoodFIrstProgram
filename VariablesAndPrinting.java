9f508c6f47065ffd6f503165cb78e1d0e07e168e
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-19 02:35:01 -0500

    Create VariablesAndPrinting.java

---
+++ VariablesAndPrinting.java
@@ -0,0 +1,23 @@
+public class VariablesAndPrinting {
+    
+    public static void main(String[] args) {
+        String hisName, herName, herFeeling, hisFeeling;
+        int herMark, hisMark, herMistakes, hisMistakes;
+        
+        hisName = "Bobby";
+        herName = "Bobbina";
+        herFeeling = "very happy";
+        hisFeeling = "very sad";
+        herMark = 48;
+        hisMark = 97;
+        herMistakes = 14;
+        hisMistakes = 2;
+        
+        System.out.println((hisName) + " and " + (herName) + " recieved their exams yesterday and compared their marks.");
+        System.out.println((herName) + " got a " + (herMark) + " and " + (hisName) + " got a " + (hisMark) + " on the exam.");
+        System.out.println("When " + (herName) + " saw her mark, she was " + (herFeeling) + " because " + (herMark) + " is her favourite number!");
+        System.out.println("When " + (hisName) + " saw his mark, he was " + (hisFeeling) + " because the number " + (hisMark) + " reminded him of how many times he was punched in the face when was a little boy.");
+        System.out.println((hisName) + " got a total of " + (hisMistakes) + " mistakes on his exam while " + (herName) + " got " + (herMistakes) + " mistakes on her exam.");
+        System.out.println("If " + (hisName) + " and " + (herName) + " add their mistakes together, it will total up to " + (herMistakes + hisMistakes) + " mistakes.");
+    }   
+}
