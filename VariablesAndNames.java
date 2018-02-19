a2b51d59518cde990a3cd637f353e53d32754c50
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-19 00:05:09 -0500

    Update VariablesAndNames.java

--- VariablesAndNames.java
+++ VariablesAndNames.java
@@ -2,20 +2,21 @@ public class VariablesAndNames {
     
     public static void main(String[] args) {
         int boxes, people, slicesPerBox;
-        double pizzaLeft, pizzaPerPerson, pizzaTotal;
+        double pizzaPerPerson, pizzaTotal, boxPrice;
         
         people = 30;
         boxes = 20;
+        boxPrice = 15;
         slicesPerBox = 15;
         pizzaTotal = (20 * 15);
         pizzaPerPerson = pizzaTotal / people;
-        pizzaLeft = 
-       
         
-        System.out.println("There are " + (pizzaTotal) + " slices of pizza for " + (people) + " people at a party.");
+        
+        System.out.println("There are " + (pizzaTotal) + " slices of pizza available for " + (people) + " people at a party.");
         System.out.println("Each person can only have " + (pizzaPerPerson) + " slices of pizza");
-        System.out.println("Bob, the party host, decided to get an extra 2 boxes of pizza for every person at the party");
-        System.out.println("2 hours later, the pizza delivery guy brought in " + (2 * 20 * 15) + " more slices of pizza");
+        System.out.println("Bob, the party host, decided to get an extra box of pizza for every 2 people at the party");
+        System.out.println("So there will be " + (pizzaTotal/2) + " extra slices available for everyone");
+        System.out.println("Since each box is $" + (boxPrice) + ", then the total price is "
         
     }
 }
