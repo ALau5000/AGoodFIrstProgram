c281dc6f1398d44c6dfaad76c6fa91f9ccfa185b
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-19 03:23:18 -0500

    Create MySchedule.java

---
+++ MySchedule.java
@@ -0,0 +1,14 @@
+public class MySchedule {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("                     Semester 2                ");
+        System.out.println("+------------------------------------------------+");
+        System.out.println("| 1 |    English             |   A. Ashworth     |");
+        System.out.println("| 2 |    Computer Science    |   D. Gallo        |");
+        System.out.println("| 3 |    Chemistry           |   A. Al-Shaikh    |");
+        System.out.println("| 4 |    Biology             |   S. Mazzacca     |");
+        System.out.println("+------------------------------------------------+");
+        
+    }
+}
