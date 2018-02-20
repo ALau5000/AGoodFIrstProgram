2d744def2c90bb69147391bfe4d27995c8209a81
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-20 11:09:12 -0500

    Update UsingVariables.java

--- UsingVariables.java
+++ UsingVariables.java
@@ -10,9 +10,9 @@ public class UsingVariables {
         coolNumber = 2.71828;
         funCourse = "Computer Science";
         
-        System.out.println("This is room # " + (roomNumber) + ".");
-        System.out.println("e is close to " + (coolNumber) + ".");
-        System.out.println("I am learning a bit about " + (funCourse) + ".");
+        System.out.println("This is room # " + roomNumber + ".");
+        System.out.println("e is close to " + coolNumber + ".");
+        System.out.println("I am learning a bit about " + funCourse + ".");
         
     }
 }
