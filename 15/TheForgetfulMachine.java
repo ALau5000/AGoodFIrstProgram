fed4c6f3b7448fe4900f004ffdd9950cedf5d0b1
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-22 11:10:29 -0500

    Create TheForgetfulMachine.java

---
+++ 15/TheForgetfulMachine.java
@@ -0,0 +1,14 @@
+import java.util.Scanner
+public class TheForgetfulMachine {
+    
+    public static void main(String[] args) {
+        Scanner poo = new Scanner(System.in);
+        
+        System.out.println("How many dogs do you have? ");
+        int numberOfDogs = input.nextInt();
+        
+        System.out.println("What is your favourite sport? ");
+        String favouriteSport = inpur.next();
+        
+    } 
+}
