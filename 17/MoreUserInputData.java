2aaa0ffe87db5e499df47f585b529de322c58fb6
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-23 11:09:10 -0500

    Create MoreUserInputData.java

---
+++ 17/MoreUserInputData.java
@@ -0,0 +1,23 @@
+public class MoreUserInputData {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Please fill out the information below so I can use for educational purposes");\
+        
+        System.out.println("First Name: ");
+        String firstName = input.next();
+        System.out.println("Last Name: ");
+        String lastName = input.next();
+        System.out.println("Grade: ");
+        int grade = input.nextInt();
+        System.out.println("Worst Mark This Year: ");
+        double worstMark = input.nextDouble();
+        System.out.println("LOL so bad, even my");
+        System.out.println("Library Card: ");
+        int libraryCard = input.nextInt();
+        System.out.println
+    }
+}
+        
+        
