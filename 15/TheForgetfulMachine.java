c4c484440c7abe5145130018a7c9b4b0a1ad2225
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-22 21:53:32 -0500

    Update TheForgetfulMachine.java

--- 15/TheForgetfulMachine.java
+++ 15/TheForgetfulMachine.java
@@ -1,14 +1,23 @@
-import java.util.Scanner
+import java.util.Scanner;
+
 public class TheForgetfulMachine {
     
     public static void main(String[] args) {
-        Scanner poo = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         
         System.out.println("How many dogs do you have? ");
         int numberOfDogs = input.nextInt();
+
+	    System.out.println("How many siblings do you have? ");
+	    int numberOfSiblings = input.nextInt();
         
         System.out.println("What is your favourite sport? ");
-        String favouriteSport = inpur.next();
+        String favouriteSport = input.next();
+
+	    System.out.println("And your favourite sports team? ");
+	    String favouriteSportsTeam = input.next();
+
+    	System.out.println("Thanks for answering these questions. I've learned a lot about you! ");
         
     } 
 }
