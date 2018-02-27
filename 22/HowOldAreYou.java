6a99fbb443a3d47c1ea5d10409e2f6b1462c32f2
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-27 10:21:15 -0500

    Create HowOldAreYou.java

---
+++ 22/HowOldAreYou.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class HowOldAreYou {
+
+    public static void main(String[] main) {
+    
+        Scanner input = new Scanner(System.in);
+
+        
+        System.out.println("Hi there, what's your name? ");
+        String name = input.next();
+        
+        System.out.println("Ok, " + name + ", how old are you?");
+        int age = input.nextInt();
+        
+        if (age < 16) {
+        System.out.println("You can't drive, " + name + ".");
+        }
+        
+        if (age < 18) {
+        System.out.println("You can't vote, " + name + ".");
+        }
+        
+        if (age < 25) {
+        System.out.println("You can't rent a car, " + name + ".");
+        }
+        
+        if (age >= 25) {
+        System.out.println("You can do anything that's legal, " + name + ".");
+        }
+    }
+}
