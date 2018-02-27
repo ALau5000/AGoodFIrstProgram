006035129d4320809d0f1eb9b888f38ca029f8c7
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-27 11:10:02 -0500

    Create SpaceBoxing.java

---
+++ 26/SpaceBoxing.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SpaceBoxing {
+    
+    public static void main(String[] args) {
+        
+            Scanner input = new Scanner(System.in);
+            
+            System.out.print("Hello, Julio, what is your current weight on earth? ");
+            double weight = input.nextDouble();
+            
+            System.out.print("I have information for the following planets:
+            
+            if (
