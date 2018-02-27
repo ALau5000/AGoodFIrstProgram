35f46336b7ab5a906908d1953c459d2a923ff951
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-26 23:11:39 -0500

    Create ADumbCalculator.java

---
+++ 19/ADumbCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ADumbCalculator {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Hey there, what's up?");
+		System.out.print("Can you give me an integer? ");
+		int firstNumber = input.nextInt();
+
+		System.out.print("And a decimal? ");
+		double secondNumber = input.nextDouble();
+
+		System.out.print("And another integer? ");
+		int thirdNumber = input.nextInt();
+		System.out.println();
+
+		System.out.println("Hmm... so your numbers combined would be... oh! I got it! It's " + (firstNumber + secondNumber + thirdNumber));
+
+	}
+} 
+
+		
