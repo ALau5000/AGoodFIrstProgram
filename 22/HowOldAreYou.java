dcb2ed6b38cf05efb51dea20f1e1db1f6a8b571b
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-27 22:51:04 -0500

    Update HowOldAreYou.java

--- 22/HowOldAreYou.java
+++ 22/HowOldAreYou.java
@@ -14,19 +14,19 @@ public class HowOldAreYou {
         int age = input.nextInt();
         
         if (age < 16) {
-        System.out.println("You can't drive, " + name + ".");
+            System.out.println("You can't drive, " + name + ".");
         }
         
         if (age < 18) {
-        System.out.println("You can't vote, " + name + ".");
+            System.out.println("You can't vote, " + name + ".");
         }
         
         if (age < 25) {
-        System.out.println("You can't rent a car, " + name + ".");
+            System.out.println("You can't rent a car, " + name + ".");
         }
         
         if (age >= 25) {
-        System.out.println("You can do anything that's legal, " + name + ".");
+            System.out.println("You can do anything that's legal, " + name + ".");
         }
     }
 }
