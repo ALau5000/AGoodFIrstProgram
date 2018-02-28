4281368bfc835e250f566ac72af56b4b0c9c3030
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-02-27 22:48:39 -0500

    Update HowOldAreYou2.java

--- 25/HowOldAreYou2.java
+++ 25/HowOldAreYou2.java
@@ -13,19 +13,19 @@ public class HowOldAreYou2 {
         int age = input.nextInt();
         
         if  (age < 16) {
-        System.out.println("You can't drive, " + name + ".");
+            System.out.println("You can't drive, " + name + ".");
         }
         
         else if (age == 16 || age == 17) {
-        System.out.println("You can drive but you can't vote, " + name + ".");
+            System.out.println("You can drive but you can't vote, " + name + ".");
         }
         
         else if (age >= 18 && age <= 24) {
-        System.out.println("You can vote but you can't rent a car, " + name + ".");
+            System.out.println("You can vote but you can't rent a car, " + name + ".");
         }
         
         else if (age >= 25) {
-        System.out.println("You can do pretty much anything, " + name + ", except illegal things.");
+            System.out.println("You can do pretty much anything, " + name + ", except illegal things.");
         }
             
     }
