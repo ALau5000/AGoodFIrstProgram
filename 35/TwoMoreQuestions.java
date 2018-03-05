23443d902bc193e4f060a0fa9cbe6ea000ce9589
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-03-05 11:10:15 -0500

    Create TwoMoreQuestions.java

---
+++ 35/TwoMoreQuestions.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TwoMoreQuestions {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("ONLY TWO MORE QUESTIONS!!!!!!!!!!!!!!!!!!!\n");
+        System.out.println("Think of something and I'll try to guess it.\n");
+        System.out.print("Questions 1) Does it stay inside or both? ");
+        String location = input.next();
+        System.out.print("Question 2) Is it a living thing? ");
+        System.out.println();
+    }
+}
+  
