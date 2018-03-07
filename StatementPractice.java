6f38cdbd9bf9cf4b9a6acd1910948e9fdbf37d49
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-03-07 10:43:14 -0500

    Create StatementPractice.java

---
+++ StatementPractice.java
@@ -0,0 +1,56 @@
+1.
+
+public class StatementPractice {
+    public static void main(String[] args) {
+        double mark;
+        
+        if (mark > 50) {
+            System.out.println("GOOD JOB YOU PASS!");
+        } else {
+            System.out.println("BETTER LUCK NEXT TIME YOU FAILED!");
+        }
+    }
+}
+        
+        
+2.  
+
+public class StatementPractice {
+    public static void main(String[] args) {
+        int x, y;
+        
+        if (x > y) {
+            System.out.println("x is greater than y!");
+        } else if (y > x) {
+            System.out.println("y is greater than x!");
+        } else {
+            System.out.println("x and y are equal!");
+        }
+    }
+}
+
+3.
+
+public class StatementPractice {
+    public static void main(String[] args) {
+        int age;
+        
+        if (age >= 65) {
+            System.out.println("senior");
+        } else if (age < 65 && age >= 18) {
+            System.out.println("regular admission");
+        } else if (age < 18 && age > 10) {
+            System.out.println("child");
+        } else {    
+            System.out.println("free");
+        }
+    }
+}
+            
+
+
+        
+        
+        
+        
+        
