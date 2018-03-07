62957e6bac0c875d537c2b541533ba06c8fa166c
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-03-07 09:56:24 -0500

    Create WorstNumberGuessingGame.java

---
+++ 40/WorstNumberGuessingGame.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class WorstNumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+        
+        System.out.println("DIS DAH WURST NUMBAR GWEUSSING GEME EVAER~!!!)@#(*&@*()_@)!(*!\n");
+        System.out.print("I_<M THUNKING UF A NOMBAR FRM 1-5! TRI TOO GEUSS IT!!!!!?!?!? ");
+        number = input.nextInt();
+        
+        System.out.println();
+        
+        if (number == 1) {
+            System.out.println("Good one, you got me! How you know? are you cheating? can you stop cheating");
+        } else {
+        System.out.println("AJAHAHAHAHAHAHHAH R U OK BUDY? U RELLY THUNK MY NUMBAR ES " + number + "?!???@@#@!?");
+        }
+    }
+}
+        
+      
+        
+        
