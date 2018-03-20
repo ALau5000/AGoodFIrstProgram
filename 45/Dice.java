a497f9437e058a919b1dd2398d55ece55894b35f
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-03-20 10:29:23 -0400

    Create Dice.java

---
+++ 45/Dice.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Dice {
+    public static void main(String[] args) {
+        Random r = new Random();
+        
+        int dice1 = 1 + r.nextInt(6);
+        int dice2 = 1 + r.nextInt(6);
+        
+        int totalRoll = dice1 + dice2;
+        
+        System.out.println("HERE COME THE DICE ROLL!!!");
+        System.out.println("DICE #1: " + dice1);
+        System.out.println("DICE #2: " + dice2);
+        System.out.println("THE TOTAL IS: " + totalRoll + "!!!");
+        
+    }
+}
