62cbf671ceddb6de0012abba9bb5dbc450b6925a
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-03-27 10:36:59 -0400

    Update AddingValues.java

--- 55/AddingValues.java
+++ 55/AddingValues.java
@@ -3,16 +3,23 @@ import java.util.Scanner;
 public class AddingValues {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int totalNumber;
+        int total = 0;
+        int addNumbers = 0;
         
         System.out.println("I will add up the numbers you give me.");
         System.out.print("Number: ");
         int number = input.nextInt();
         
         while (number != 0) {
-            totalNumber = number;   
-            System.out.println("The total so far is " + totalNumber + number );
-            System.out.print("Number: ");
+            addNumbers = number + total ;   
+            total = addNumbers; 
+            System.out.println("The total so far is " + total);
+            System.out.print("Number: ");   
+            number = input.nextInt();  
+            
+            if (number == 0) {
+                System.out.println("Your total is " + total);
+            }
         }
     }
 }
