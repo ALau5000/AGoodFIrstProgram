34265bb6dfce444226d395f271eba0f50fe639e7
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-03-27 10:44:18 -0400

    Create TenTimes.java

---
+++ 65/TenTimes.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TenTimes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        for (int msg = 1; msg <= 10; msg += 1) {
+            System.out.println(msg + ". Mr.Gallo is cool.");
+        }
+    }
+}
+            
+        
