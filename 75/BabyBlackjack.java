1b1cb691f86d8639520eb2837782ee1ebd67847d
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-03-28 10:25:57 -0400

    Create BabyBlackjack.java

---
+++ 75/BabyBlackjack.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class Main {
+	public static void main(String[] args) {
+	  Random r = new Random();
+	  
+	  int card1 = 1 + r.nextInt(10);
+	  int card2 = 1 + r.nextInt(10);
+	  int card3 = 1 + r.nextInt(10);
+	  int card4 = 1 + r.nextInt(10);
+	  int playerTotal = card1 + card2;
+	  int dealerTotal = card3 + card4;
+	  
+	  System.out.println("Baby Blackjack!!!\n");
+	  System.out.println("You drew " + card1 + " and " + card2 + "!");
+	  System.out.println("Your total is " + (playerTotal) + "!\n");
+	  System.out.println("The dealer has " + card3 + " and " + card4 + "!");
+	  System.out.println("The Dealer's total is " + (dealerTotal) + "!\n");
+	  
+	  if (playerTotal > dealerTotal) {
+	    System.out.println("YOU WIN! :D");
+	  } else if (dealerTotal > playerTotal) {
+	    System.out.println("YOU LOSE! :(");
+	  } else {
+	    System.out.println("YOU TIED!!! :/");
+	  }
+	}
+}
