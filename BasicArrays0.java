b84a054f8240620ea8e407b3346f9d609cade7b0
ALau5000 <36391914+ALau5000@users.noreply.github.com>
2018-04-16 22:35:10 -0400

    Create BasicArrays0.java

---
+++ BasicArrays0.java
@@ -0,0 +1,27 @@
+public class BasicArrays0 {
+  public static void main(String[] args) {
+    
+    int[] tenInt = new int[10];
+    tenInt[0] = -113;
+    tenInt[1] = -113;
+    tenInt[2] = -113;
+    tenInt[3] = -113;
+    tenInt[4] = -113;
+    tenInt[5] = -113;
+    tenInt[6] = -113;
+    tenInt[7] = -113;
+    tenInt[8] = -113;
+    tenInt[9] = -113;
+    
+    System.out.println(tenInt[0]);
+    System.out.println(tenInt[1]);
+    System.out.println(tenInt[2]);
+    System.out.println(tenInt[3]);
+    System.out.println(tenInt[4]);
+    System.out.println(tenInt[5]);
+    System.out.println(tenInt[6]);
+    System.out.println(tenInt[7]);
+    System.out.println(tenInt[8]);
+    System.out.println(tenInt[9]);
+  }
+}
